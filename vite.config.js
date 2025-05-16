d742f408e3104ba6b10e2f7ba27bf7e50de7ca14
jaswindersingh78 <jassstark78@gmail.com>
2025-05-17 00:10:42 +0530

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+
   base: '/Headline-Hub/', // 👈 Use your repo name here
 })
 
